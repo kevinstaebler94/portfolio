f740b28823d339180e92e081ad258d2020bf7473
Kevin <kevinstaebler1994@outlook.de>
2026-01-19 22:11:30 +0100

    about me section has been implemented but is still in progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
-import { LandingComponent } from "./features/landing/landing/landing.component";
+import { LandingComponent } from "./features/landing/landing.component";
+import { AboutComponent } from "./features/about/about.component";
 
 @Component({
   selector: "app-root",
-  imports: [RouterOutlet, LandingComponent],
+  imports: [RouterOutlet, LandingComponent, AboutComponent],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.scss",
 })
--- src/app/features/landing/landing.component.ts
+++ src/app/features/landing/landing.component.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-import { HeaderComponent } from "../../../shared/components/header/header.component";
+import { HeaderComponent } from "../../shared/components/header/header.component";
 
 @Component({
   selector: "app-landing",
