bcc49e9e9fff731492c7a1e8871088667fe84d98
Kevin <kevinstaebler1994@outlook.de>
2026-01-22 22:09:44 +0100

    skill set html structure has been implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { LandingComponent } from "./features/landing/landing.component";
 import { AboutComponent } from "./features/about/about.component";
+import { SkillsComponent } from "./features/skills/skills.component";
 
 @Component({
   selector: "app-root",
-  imports: [RouterOutlet, LandingComponent, AboutComponent],
+  imports: [RouterOutlet, LandingComponent, AboutComponent, SkillsComponent],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.scss",
 })
