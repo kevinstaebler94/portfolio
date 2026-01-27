eb184be8bbdbac5d63b1c7af4ce35a7db5201e4e
Kevin <kevinstaebler1994@outlook.de>
2026-01-27 21:12:11 +0100

    adjusted size of the whole page and added an data array for the about me page

---
+++ src/app/data/about-data.ts
@@ -0,0 +1,30 @@
+export const aboutData = [
+  {
+    icon: "assets/icons/location-work_model.svg",
+    text: "Based in Germany and open to remote or on-site work.",
+  },
+  {
+    icon: "assets/icons/tech-stack.svg",
+    text: "I use HTML, CSS, JavaScript, TypeScript and Angular to build modern web applications. I also work with Firebase, GIT and REST APIs",
+  },
+  {
+    icon: "assets/icons/motivation-coding.svg",
+    text: "I enjoy turning ideas into real, working applications and learning something new with every project.",
+  },
+  {
+    icon: "assets/icons/learning-growth.svg",
+    text: "After 14 years in logistics, I decided to take a new path and move into IT. I’m always eager to learn and improve my skills step by step.",
+  },
+  {
+    icon: "assets/icons/work-ethic-mindset.svg",
+    text: "Focused, motivated and enjoying the process, while always keeping my goals in sight.",
+  },
+  {
+    icon: "assets/icons/soft-skills.svg",
+    text: "Team player, communicative, solution-oriented and easy to work with.",
+  },
+  {
+    icon: "assets/icons/personal-drive.svg",
+    text: "Using this phase of my 30s to step out of my comfort zone and build a solid future in the IT world.",
+  },
+];
